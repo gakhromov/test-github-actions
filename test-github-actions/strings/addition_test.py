cdb8c9ba276b14bc082e239054b2a5d733e3926f
Grigory Khromov <grigory@foreai.co>
2023-12-28 11:40:19 +0100

    upd

--- test-github-actions/strings/addition_test.py
+++ test-github-actions/strings/addition_test.py
@@ -8,7 +8,7 @@ class TestAddStrings(unittest.TestCase):
 
     def test_add_empty_string(self):
         with self.assertRaises(ValueError):
-            add_strings('', 'Hello')
+            add_strings('', 'World')
 
 
 if __name__ == '__main__':
