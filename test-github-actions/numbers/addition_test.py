4e5155f069c7ae1a9bff4dd608a10f412e6313be
Grigory Khromov <grigory@foreai.co>
2023-12-28 14:37:06 +0100

    test: add abseil tests

--- test-github-actions/numbers/addition_test.py
+++ test-github-actions/numbers/addition_test.py
@@ -1,12 +1,14 @@
-import unittest
+from absl.testing import absltest, parameterized
 from numbers.addition import add_floats, add_integers
 
 
-class TestAddition(unittest.TestCase):
-    def test_addition(self):
-        self.assertEqual(add_integers(1, 1), 2)
-        self.assertEqual(add_floats(1.0, 1.0), 2.0)
+class TestAddition(parameterized.TestCase):
+
+    @parameterized.parameters((1, 1, 2), (5, 6, 11))
+    def test_addition(self, a, b, c):
+        self.assertEqual(add_integers(a, b), c)
+        self.assertEqual(add_floats(float(a), float(b)), float(c))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    absltest.main()
